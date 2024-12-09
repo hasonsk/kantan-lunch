6c43943cfaf841943f19271d00ddea411ae3adc6
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-12-09 19:23:02 +0700

    modify data

--- frontend/src/pages/admin/reviewData.jsx
+++ frontend/src/pages/admin/reviewData.jsx
@@ -1,7 +1,7 @@
 const postData = [
     {
         id: 1,
-        user: "サニー・ビストロ",
+        user: "Sam",
         media: ["https://images2.thanhnien.vn/528068263637045248/2023/2/28/9afcb59ff8f622a87be7-16760314638681857498218-16775749875331071087079.jpeg", "https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2023/6/11/1203369/IMG_1612.JPG"],
         content: "ハッピータウン、サンシャインアベニュー123",
         created: "20/10/2024",
@@ -9,8 +9,8 @@ const postData = [
         reviewed: false
     },
     {
-        id: 1,
-        user: "サニー・ビストロ",
+        id: 2,
+        user: "Sarah",
         media: ["https://images2.thanhnien.vn/528068263637045248/2023/2/28/9afcb59ff8f622a87be7-16760314638681857498218-16775749875331071087079.jpeg", "https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2023/6/11/1203369/IMG_1612.JPG"],
         content: "ハッピータウン、サンシャインアベニュー123",
         created: "20/10/2024",
