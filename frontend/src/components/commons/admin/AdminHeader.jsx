d269bec577e70d1b5e8a01f5b724154a8f04a535
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-12-09 18:05:50 +0700

    package.json

---
+++ frontend/src/components/commons/admin/AdminHeader.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../Header.css';
+
+const AdminHeader = () => {
+    return (
+        <div className='header-wrapper'>
+            <header className="header">
+                {/* Logo bên trái */}
+                <div className="logo">
+                    {/* <img src="/src/assets/logo.png" alt="Logo" className="logo-img" /> Thay 'logo.png' bằng đường dẫn logo của bạn */}
+                    LOGO
+                </div>
+
+                {/* Các icon bên phải */}
+                <div className="icons">
+                    <a href="/notifications" className="icon">
+                        <i className="fa-regular fa-bell"></i> {/* Icon thông báo dạng outline */}
+                    </a>
+                    <a href="/profile" className="icon">
+                        <i className="fa-regular fa-user"></i> {/* Icon người dùng dạng outline */}
+                    </a>
+                </div>
+            </header>
+
+            {/* Thêm nav bên dưới header */}
+            <nav className="sub-nav">
+                <a href="/user" className="sub-nav-link active">レビュー管理</a>
+                <a href="/review" className="sub-nav-link">コンテンツ管理ページ</a>
+                <a href="/menu" className="sub-nav-link">食品およびレストラン</a>
+            </nav>
+        </div>
+    );
+};
+
+export default AdminHeader;
