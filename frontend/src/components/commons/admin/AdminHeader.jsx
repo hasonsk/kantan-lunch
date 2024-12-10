7d894c6c6416e10ba7a43d98a1e1b57090babb66
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-12-10 13:06:52 +0700

    done admin header prototype

--- frontend/src/components/commons/admin/AdminHeader.jsx
+++ frontend/src/components/commons/admin/AdminHeader.jsx
@@ -11,23 +11,29 @@ const AdminHeader = () => {
                     LOGO
                 </div>
 
+                <nav className="sub-nav">
+                    <a href="/admin/user" className="sub-nav-link active">レビュー管理</a>
+                    <a href="/admin/review" className="sub-nav-link">コンテンツ管理ページ</a>
+                    <a href="/admin/menu" className="sub-nav-link">食品およびレストラン</a>
+                </nav>
+
                 {/* Các icon bên phải */}
                 <div className="icons">
-                    <a href="/notifications" className="icon">
-                        <i className="fa-regular fa-bell"></i> {/* Icon thông báo dạng outline */}
-                    </a>
+                    {/*<a href="/notifications" className="icon">*/}
+                    {/*    <i className="fa-regular fa-bell"></i> /!* Icon thông báo dạng outline *!/*/}
+                    {/*</a>*/}
                     <a href="/profile" className="icon">
-                        <i className="fa-regular fa-user"></i> {/* Icon người dùng dạng outline */}
+                        <div className="display-flex-30">
+                            <div>
+                                <i className="fa-regular fa-user"></i> {/* Icon người dùng dạng outline */}
+                            </div>
+                            <span>
+                                Hello, Admin
+                            </span>
+                        </div>
                     </a>
                 </div>
             </header>
-
-            {/* Thêm nav bên dưới header */}
-            <nav className="sub-nav">
-                <a href="/admin/user" className="sub-nav-link active">レビュー管理</a>
-                <a href="/admin/review" className="sub-nav-link">コンテンツ管理ページ</a>
-                <a href="/admin/menu" className="sub-nav-link">食品およびレストラン</a>
-            </nav>
         </div>
     );
 };
