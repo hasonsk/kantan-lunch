edeb3b61ebc09028569220dff634c096f71023db
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-12-10 13:17:29 +0700

    fix overflow header

--- frontend/src/pages/admin/ReviewManagement.jsx
+++ frontend/src/pages/admin/ReviewManagement.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 import './ReviewManagement.css';
 import postData from "./reviewData.jsx"
-import { getAllPosts } from '../../api/post';
+import {getAllPosts} from '../../api/post';
 
 const ReviewManagement = () => {
     const [searchQuery, setSearchQuery] = useState('');
@@ -25,7 +25,7 @@ const ReviewManagement = () => {
     };
 
     useEffect(() => {
-         // fetchPosts();
+        // fetchPosts();
     }, []);
 
     // const renderStars = (rating) => {
@@ -59,9 +59,9 @@ const ReviewManagement = () => {
             <div className="search-bar">
                 <p className="greeting-text">レビュー</p>
                 <div className="input-container">
-          <span className="search-icon">
-            <i className="fa fa-search"></i>
-          </span>
+                    <span className="search-icon">
+                        <i className="fa fa-search"></i>
+                    </span>
                     <input
                         type="text"
                         placeholder="検索"
@@ -72,37 +72,37 @@ const ReviewManagement = () => {
                 </div>
             </div>
 
-            <div className="main-content">
-                {postData.length > 0 ? (
-                    <div className="post-list">
-                        {postData.map((post) => {
-                            return (
-                                <div key={post.id} className="post-card">
-                                    <div className="post-header">
-                                        <div className="post-avatar">
-                                            <img src={post.media[0] || 'default-avatar.jpg'} alt="User Avatar"/>
-                                        </div>
-                                        <div className="post-user-info">
-                                            <h3>{post.user}</h3>
-                                            <p>総レビュー数: {post.reviewCount || 0}</p>
-                                        </div>
-                                    </div>
-                                    <div className="post-body">
-                                        <p className="post-date">{post.created}</p>
-                                        <p className="post-content">{post.content}</p>
-                                        <div className="post-footer">
-                                            <button className="reject-button">拒否する</button>
-                                            <button className="accept-button">受け入れる</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            );
-                        })}
-                    </div>
-                ) : (
-                    <p className="no-results">該当するレストランが見つかりませんでした。</p>
-                )}
-            </div>
+            {/*<div className="main-content">*/}
+            {/*    {postData.length > 0 ? (*/}
+            {/*        <div className="post-list">*/}
+            {/*            {postData.map((post) => {*/}
+            {/*                return (*/}
+            {/*                    <div key={post.id} className="post-card">*/}
+            {/*                        <div className="post-header">*/}
+            {/*                            <div className="post-avatar">*/}
+            {/*                                <img src={post.media[0] || 'default-avatar.jpg'} alt="User Avatar"/>*/}
+            {/*                            </div>*/}
+            {/*                            <div className="post-user-info">*/}
+            {/*                                <h3>{post.user}</h3>*/}
+            {/*                                <p>総レビュー数: {post.reviewCount || 0}</p>*/}
+            {/*                            </div>*/}
+            {/*                        </div>*/}
+            {/*                        <div className="post-body">*/}
+            {/*                            <p className="post-date">{post.created}</p>*/}
+            {/*                            <p className="post-content">{post.content}</p>*/}
+            {/*                            <div className="post-footer">*/}
+            {/*                                <button className="reject-button">拒否する</button>*/}
+            {/*                                <button className="accept-button">受け入れる</button>*/}
+            {/*                            </div>*/}
+            {/*                        </div>*/}
+            {/*                    </div>*/}
+            {/*                );*/}
+            {/*            })}*/}
+            {/*        </div>*/}
+            {/*    ) : (*/}
+            {/*        <p className="no-results">該当するレストランが見つかりませんでした。</p>*/}
+            {/*    )}*/}
+            {/*</div>*/}
         </div>
     );
 };
